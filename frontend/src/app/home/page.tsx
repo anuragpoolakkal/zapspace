5d40a56a1c928c545e10d4f0f3082e1d6ff2aa96
Aqeel <aqeelten@gmail.com>
2023-06-24 15:05:31 +0530

    b : business model and routes

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,31 +1,96 @@
 "use client";
 import { useState } from "react";
-import { FiX } from "react-icons/fi";
+import { FiX, FiUser } from "react-icons/fi";
 import Image from "next/image";
+import { serverURL } from "@/utils/util";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export default function Home() {
     const [showOnboarding, setShowOnboarding] = useState(false);
     const [showSignUp, setShowSignUp] = useState(false);
     const [showLogin, setShowLogin] = useState(false);
 
+    const [signUpData, setSignUpData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const [loginData, setLoginData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const signUp = async () => {
+        const config = {
+            method: "POST",
+            url: `${serverURL}/user/signup`,
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+            },
+            data: signUpData
+        };
+
+        axios(config).then((response) => {
+            console.log(response.data);
+            toast.success("Signed Up Successfully");
+            setShowSignUp(false);
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
+
+
+    const login = async () => {
+        const config = {
+            method: "POST",
+            url: `${serverURL}/user/login`,
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+            },
+            data: loginData
+        };
+
+        axios(config).then((response) => {
+            console.log(response.data);
+            localStorage.setItem("user", JSON.stringify(response.data.user));
+            localStorage.setItem("token", response.data.token);
+            toast.success("Logged In Successfully");
+            setShowLogin(false);
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
+
     return <div className="flex w-screen h-screen bg-white flex flex-col text-black p-5">
         <nav className="flex justify-between">
             <p className="text-lg font-bold">Zapspace</p>
-            <button onClick={() => setShowOnboarding(true)} className="btn btn-primary">Login</button>
+            {localStorage.getItem("user") ? <div className="flex items-center"><FiUser onClick={() => {
+                localStorage.clear();
+                window.location.reload()
+            }} className="cursor-pointer text-2xl mr-2" /><p>{JSON.parse(localStorage.getItem("user")!).name}</p></div> : <button onClick={() => setShowLogin(true)} className="btn btn-primary">Login</button>}
         </nav>
         {/* SignUp */}
         {showSignUp ? <main className="absolute left-0 top-0 w-full h-full flex flex-col bg-[#DDC12D] p-5">
             <nav className="flex w-full justify-between">
                 <p className="text-lg font-bold">Zapspace</p>
-                <FiX onClick={() => setShowOnboarding(false)} className="cursor-pointer text-2xl" />
+                <FiX onClick={() => setShowSignUp(false)} className="cursor-pointer text-2xl" />
             </nav>
-            <div className="flex px-8 h-full items-center justify-center">
+            <div className="flex px-8 h-full items-center justify-center mb-20">
                 <div className="w-full h-full flex flex-col justify-center">
-                    <p>1 of 6</p>
-                    <p className="text-2xl font-semibold mb-3">Start your business in a few clicks</p>
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    <p className="text-2xl mb-20 font-bold">SignUp</p>
+                    <p className="text-lg font-semibold mb-3">Name</p>
+                    <input type="text" onChange={(x) => setSignUpData({ ...signUpData, name: x.target.value })} placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
+                    <p className="text-lg font-semibold mb-3 mt-3">Email</p>
+                    <input type="email" onChange={(x) => setSignUpData({ ...signUpData, email: x.target.value })} placeholder="Your Email" className="input input-bordered w-full max-w-xs" />
+                    <p className="text-lg font-semibold mb-3 mt-3">Password</p>
+                    <input type="password" onChange={(x) => setSignUpData({ ...signUpData, password: x.target.value })} placeholder="Set a Password" className="input input-bordered w-full max-w-xs" />
                     <div className="flex">
-                        <button className="btn btn-neutral mt-7">NEXT</button>
+                        <button onClick={(x) => signUp()} className="btn btn-neutral mt-7">Signup</button>
                     </div>
                 </div>
                 <Image src="https://cdn3d.iconscout.com/3d/premium/thumb/shopping-store-5130510-4292743.png" alt="icon" width={500} height={500} />
@@ -36,15 +101,17 @@ export default function Home() {
         {showLogin ? <main className="absolute left-0 top-0 w-full h-full flex flex-col bg-[#DDC12D] p-5">
             <nav className="flex w-full justify-between">
                 <p className="text-lg font-bold">Zapspace</p>
-                <FiX onClick={() => setShowOnboarding(false)} className="cursor-pointer text-2xl" />
+                <FiX onClick={() => setShowLogin(false)} className="cursor-pointer text-2xl" />
             </nav>
-            <div className="flex px-8 h-full items-center justify-center">
+            <div className="flex px-8 h-full items-center justify-center mb-20">
                 <div className="w-full h-full flex flex-col justify-center">
-                    <p>1 of 6</p>
-                    <p className="text-2xl font-semibold mb-3">Start your business in a few clicks</p>
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    <p className="text-2xl mb-20 font-bold">Login</p>
+                    <p className="text-lg font-semibold mb-3">Email</p>
+                    <input type="text" onChange={(x) => setLoginData({ ...loginData, email: x.target.value })} placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    <p className="text-lg font-semibold mb-3 mt-3">Password</p>
+                    <input type="password" onChange={(x) => setLoginData({ ...loginData, password: x.target.value })} placeholder="Type here" className="input input-bordered w-full max-w-xs" />
                     <div className="flex">
-                        <button className="btn btn-neutral mt-7">NEXT</button>
+                        <button onClick={(x) => login()} className="btn btn-neutral mt-7">Login</button>
                     </div>
                 </div>
                 <Image src="https://cdn3d.iconscout.com/3d/premium/thumb/shopping-store-5130510-4292743.png" alt="icon" width={500} height={500} />
@@ -72,7 +139,8 @@ export default function Home() {
         {/* Onboarding */}
         <main className="w-full h-full flex flex-col justify-center items-center">
             <p className="text-2xl font-semibold">Start your business in a few clicks</p>
-            <button onClick={() => setShowOnboarding(true)} className="btn btn-neutral mt-7">GET STARTED</button>
+            <button onClick={() => setShowSignUp(true)} className="btn btn-neutral mt-7">GET STARTED</button>
         </main>
+        <ToastContainer />
     </div>;
 }
