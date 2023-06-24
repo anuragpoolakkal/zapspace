008bd93dea024e5d28432779becf983bcf2e9359
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-06-24 18:27:54 +0530

    updated chat

--- frontend/src/app/Chat/page.tsx
+++ frontend/src/app/Chat/page.tsx
@@ -1,12 +1,24 @@
-import Chat from "../component/Chat";
+"use client";
+
+import Chat from "./component/Chat";
+import { useState } from "react";
 
 export default function ChatPage() {
+    const [close, setClose] = useState(false);
+
     return (
         <div className="h-screen flex bg-white ">
             <div className="p-5 h-min w-full">
                 <h6 className="text-6xl font-bold text-black">Chat</h6>
-                <Chat />
-                
+                {!close &&
+                    <Chat onClose={
+                        () => {
+                            setClose(true);
+                        }
+                    } />
+
+                }
+
             </div>
 
         </div>
