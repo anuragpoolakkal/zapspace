ec0f5f2a9ae370e223f5b81983be31e9ae355e6a
Anurag <anuragrajanp@gmail.com>
2023-06-24 17:43:54 +0530

    category

--- frontend/src/utils/util.ts
+++ frontend/src/utils/util.ts
@@ -1,19 +1,26 @@
 const serverURL = "http://localhost:6001";
 
 const onboardingMessages = [
-  {
-    title: "What is the name of your business?",
-  },
-  {
-    title: "Describe your business in a few words.",
-  },
-  {
-    title: "What category does your business fall under?",
-  },
+	{
+		title: "What is the name of your business?",
+	},
+	{
+		title: "Describe your business in a few words.",
+	},
+	{
+		title: "What category does your business fall under?",
+	},
 ];
 
 const categories = [
-    
+	"Pottery",
+	"Weavery",
+	"Handmade Crafts",
+	"Textile Art",
+	"Food industry",
+	"Bottle Art",
+	"Home Decor Crafts",
+	"Plants",
 ];
 
 export { serverURL, onboardingMessages };
